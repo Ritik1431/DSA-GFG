78e443728a29153900468545f7a64579cad6932b
Ritik1431 <ritik221jaggi@gmail.com>
2022-10-17 20:32:41 +0530

    done

--- 6_Sorting/19_lemuto_partition.cpp
+++ 6_Sorting/19_lemuto_partition.cpp
@@ -4,7 +4,7 @@ int lemuto_partition(int arr[], int l,int h)
 {
     int pivot = arr[h];  // Always last element.
     int i = l-1;
-    for (int j = l; i < h-1; j++)
+    for (int j = l; j <= h-1; j++)
     {
         if(arr[j]<pivot)
         {
@@ -12,13 +12,17 @@ int lemuto_partition(int arr[], int l,int h)
             swap(arr[i],arr[j]);
         }
     }
-    swap(arr[i+1],pivot);
+    swap(arr[i+1],arr[h]);
     return (i+1);
 }
 int main()
 {
     int arr[] = {10,80,30,90,40,50,70};
-    int h = sizeof(arr)/sizeof(arr[0]);
-    lemuto_partition(arr,0,h);
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<lemuto_partition(arr,0,n-1)<<endl;
+    for(int x : arr)
+    {
+        cout<<x<<" ";
+    }
     return 0;
 }
