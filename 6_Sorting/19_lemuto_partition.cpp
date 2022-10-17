6104a100c4c817b85673b4852ed79b19660583cb
Ritik1431 <ritik221jaggi@gmail.com>
2022-10-17 10:46:57 +0530

    done

---
+++ 6_Sorting/19_lemuto_partition.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lemuto_partition(int arr[], int l,int h)
+{
+    int pivot = arr[h];  // Always last element.
+    int i = l-1;
+    for (int j = l; i < h-1; j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],pivot);
+    return (i+1);
+    
+
+}
+int main()
+{
+    int arr[] = {10,80,30,90,40,50,70};
+    int h = sizeof(arr)/sizeof(arr[0]);
+    lemuto_partition(arr,0,h);
+    return 0;
+}
