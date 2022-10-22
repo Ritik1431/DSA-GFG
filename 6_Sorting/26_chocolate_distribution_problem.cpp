9e4935a4420e7b12d6e584ac6f8ba215b6d38bec
Ritik1431 <ritik221jaggi@gmail.com>
2022-10-22 14:43:36 +0530

    done

--- 6_Sorting/26_chocolate_distribution_problem.cpp
+++ 6_Sorting/26_chocolate_distribution_problem.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int choc_distribution(int arr[], int n, int m)  // Here m is no of children.
+{
+    if(m>n)
+        return -1;
+    sort(arr,arr + n);
+    int res = arr[m-1] - arr[0];
+    for (int i = 1;(i+m-1) < n; i++)
+    {
+        res = min(res,arr[i+m-1] - arr[i]);
+    }
+    return res;
+}
 int main()
 {
-    
+    int arr[] = {7,3,1,8,9,12,56};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int m = 3;
+    cout<<choc_distribution(arr,n,m);
+
     return 0;
 }
