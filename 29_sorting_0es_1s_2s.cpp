07616c59979e45b2af0e0739f6d80250d52ca385
Ritik1431 <ritik221jaggi@gmail.com>
2022-10-22 22:15:06 +0530

    done

--- 29_sorting_0es_1s_2s.cpp
+++ 29_sorting_0es_1s_2s.cpp
@@ -1,17 +1,35 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>    // Dutch National Flag Algorithm
 using namespace std;
-void sorting_0_1_2(int arr[], int n, int l , int h)
+void sorting_0_1_2(int arr[], int n)
 {
-    int i = l, j = h;
-    while(true)
+    int  low = 0, mid = 0, high = n-1;
+    while (mid<=high)
     {
-        if(arr[i]<)
+        switch (arr[mid])
+        {
+        case 0:
+        swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+            break;
+        
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(arr[high], arr[mid]);
+            high--;
+            break;
+        }
     }
-    
-    
+ 
 }
 int main()
 {
-    
+    int arr[] = {1,0,2,0,1,2};
+    int n = 6;
+    sorting_0_1_2(arr,n); 
+    for(int x :arr)
+        cout<<x<<" ";
     return 0;
 }
